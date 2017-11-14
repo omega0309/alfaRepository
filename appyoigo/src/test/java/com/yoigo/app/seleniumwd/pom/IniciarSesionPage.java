f90678b42ecf3bcd34fe68599bebe17767b608c5
karli_000 <karli_000@Lenovo-PC>
2017-11-14 12:47:25 +0100

    Se ha creado el paquete pom y la clase IniciarSesionPage

---
+++ appyoigo/src/test/java/com/yoigo/app/seleniumwd/pom/IniciarSesionPage.java
@@ -0,0 +1,106 @@
+package com.yoigo.app.seleniumwd.pom;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.yoigo.app.seleniumwd.pom.driver.appYoigoDriver;
+
+import io.appium.java_client.android.AndroidDriver;
+
+
+public class IniciarSesionPage {
+	private By botonIngresar = By.id("com.yoigo.movil:id/btnLogin");
+	private By botonAceptar = By.id("com.yoigo.movil:id/btnPositive");
+	private By botonPermitir = By.id("com.android.packageinstaller:id/permission_allow_button");
+	private By cajaUsuario = By.id("com.yoigo.movil:id/etNumber");
+	private By cajaClave = By.id("com.yoigo.movil:id/etPassword");
+	private By mensajeRespuesta = By.id("com.yoigo.movil:id/txtMessage");
+	private By mensajeBienvenida = By.id("com.yoigo.movil:id/consumptions_greeting");
+	private AndroidDriver driver = null;
+	
+	public IniciarSesionPage() throws MalformedURLException, InterruptedException{
+		driver = appYoigoDriver.inicializarDriver();
+	}
+
+	public String iniciarSesion(String usuario, String clave) throws Exception{	
+/*		if (isAlertPresent()) {
+			WebDriverWait wait = new WebDriverWait(driver, 3);
+			wait.until(ExpectedConditions.alertIsPresent());
+			driver.switchTo().alert().accept();
+			Thread.sleep(3000);
+		}*/		
+//		boolean present;
+		try {
+		   driver.findElement(botonPermitir).click();
+		   Thread.sleep(3000);
+//		   present = true;
+		} catch (NoSuchElementException e) {
+//		   present = false;
+		}
+		driver.findElement(botonPermitir).click();
+		Thread.sleep(3000);
+		driver.findElement(cajaUsuario).clear();
+		driver.findElement(cajaUsuario).sendKeys(usuario);
+		driver.findElement(cajaClave).clear();
+		driver.findElement(cajaClave).sendKeys(clave);
+		driver.findElement(botonIngresar).click();
+		Thread.sleep(3000);
+		/*findElements will return an empty list if no matching elements are found instead of an exception
+		This will return true if at least one element is found and false if it does not exist.
+		there's an implicit 3 second wait if no elements are found which can be switched on/off*/
+		driver.manage().timeouts().implicitlyWait(0, TimeUnit.MILLISECONDS);
+		Boolean isPresent = driver.findElements(mensajeRespuesta).size() > 0;
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		
+		if (isPresent) {
+			String mensaje= driver.findElement(mensajeRespuesta).getText();
+			driver.findElement(botonAceptar).click();
+			return mensaje;
+			
+		}
+		else {
+			return driver.findElement(mensajeBienvenida).getText();
+		}
+	}
+	
+	public boolean isAlertPresent() 
+	{ 
+	    try 
+	    { 
+	        driver.switchTo().alert(); 
+	        return true; 
+	    }   // try 
+	    catch (NoAlertPresentException Ex) 
+	    { 
+	        return false; 
+	    }   // catch 
+	}   // isAlertPresent()
+	
+	public boolean isPermisoAlertPresent() 
+	{ 
+	    try 
+	    { 
+	    	driver.findElement(botonAceptar); 
+	        return true; 
+	    }   // try 
+	    catch (NoAlertPresentException Ex) 
+	    { 
+	        return false; 
+	    }   // catch 
+	}   // isAlertPresent()
+	
+	
+	public void cerrarSesion(){
+		appYoigoDriver.terminarSesion(driver);
+	}
+	
+	public AndroidDriver getWebDriver() {
+		return driver;
+	}
+	
+}
